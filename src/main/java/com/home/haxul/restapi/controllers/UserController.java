19d670683b231297d2d7e7a4e641ca186f71f539
haxul <s.s.starodubov@yandex.ru>
2019-10-29 21:43:25 +0400

    create simple controller

--- src/main/java/com/home/haxul/restapi/controllers/UserController.java
+++ src/main/java/com/home/haxul/restapi/controllers/UserController.java
@@ -1,15 +1,18 @@
 package com.home.haxul.restapi.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
 
+    @GetMapping(path = "/{userId}")
+    public String getPage(@PathVariable String userId) {
+        return userId;
+    }
+
     @GetMapping
-    public String getPage() {
-        return "hello spring";
+    public String getPage(@RequestParam(value = "limit") int limit, @RequestParam(value = "page", defaultValue = "200") int page) {
+        return "page: " + page + " ==> " + " limit " + limit;
     }
 }
