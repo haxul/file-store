510b9f6937eec4eaaa4ce9e016a9e1969a31baaf
haxul <s.s.starodubov@yandex.ru>
2019-10-29 22:24:31 +0400

    create simple controller

--- src/main/java/com/home/haxul/restapi/controllers/UserController.java
+++ src/main/java/com/home/haxul/restapi/controllers/UserController.java
@@ -1,6 +1,12 @@
 package com.home.haxul.restapi.controllers;
 
+import com.home.haxul.restapi.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("users")
@@ -14,5 +20,23 @@ public class UserController {
     @GetMapping
     public String getPage(@RequestParam(value = "limit") int limit, @RequestParam(value = "page", defaultValue = "200") int page) {
         return "page: " + page + " ==> " + " limit " + limit;
+    }
+
+    @GetMapping(value = "/some")
+    public ResponseEntity<User> getPage() {
+        User user = new User();
+        user.setAge(28);
+        user.setLastname("starodubov");
+        user.setName("sergey");
+        return new ResponseEntity<User>(user, HttpStatus.valueOf(201));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> getUser(@RequestBody User user) {
+        User account = new User();
+        account.setName(user.getName());
+        account.setLastname(user.getLastname());
+        account.setAge(user.getAge());
+        return new ResponseEntity<User>(account, HttpStatus.OK);
     }
 }
