4a7781f31173f2f689cb624313fc95eb67420203
haxul <s.s.starodubov@yandex.ru>
2019-10-30 22:48:32 +0400

    next step

--- src/main/java/com/home/haxul/restapi/controllers/UserController.java
+++ src/main/java/com/home/haxul/restapi/controllers/UserController.java
@@ -7,14 +7,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
 
+    private HashMap<String, User> store = new HashMap<>();
+
     @GetMapping(path = "/{userId}")
-    public String getPage(@PathVariable String userId) {
-        return userId;
+    public ResponseEntity<User> getPage(@PathVariable String userId) {
+        User user = (User) store.get(userId);
+        if (user != null) return new ResponseEntity<User>(user, HttpStatus.OK);
+        return new ResponseEntity<User>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping
@@ -25,18 +32,34 @@ public class UserController {
     @GetMapping(value = "/some")
     public ResponseEntity<User> getPage() {
         User user = new User();
-        user.setAge(28);
+        user.setEmail("starodubov@yandex.ru");
+        user.setPassword("123");
         user.setLastname("starodubov");
         user.setName("sergey");
         return new ResponseEntity<User>(user, HttpStatus.valueOf(201));
     }
 
     @PostMapping
-    public ResponseEntity<User> getUser(@RequestBody User user) {
+    public ResponseEntity<User> getUser(@Valid @RequestBody User user) {
         User account = new User();
         account.setName(user.getName());
         account.setLastname(user.getLastname());
-        account.setAge(user.getAge());
+        account.setPassword(user.getPassword());
+        account.setEmail(user.getEmail());
+        String uid = UUID.randomUUID().toString();
+        account.setId(uid);
+        store.put(uid, account);
+        return new ResponseEntity<User>(account, HttpStatus.OK);
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable String id, @RequestBody User user) {
+        User account = store.get(id);
+        if (account == null) return new ResponseEntity<User>(HttpStatus.NO_CONTENT);
+        if (user.getEmail() != null) account.setEmail(user.getEmail());
+        if (user.getLastname() != null) account.setLastname(user.getLastname());
+        if (user.getPassword() != null) account.setPassword(user.getPassword());
+        if (user.getName() != null) account.setName(user.getName());
         return new ResponseEntity<User>(account, HttpStatus.OK);
     }
 }
--- src/main/java/com/home/haxul/restapi/dto/User.java
+++ src/main/java/com/home/haxul/restapi/dto/User.java
@@ -1,9 +1,47 @@
 package com.home.haxul.restapi.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+    @NotNull(message = "name is null")
     private String name;
+    @NotNull(message = "lastname is null")
     private String lastname;
-    private int age;
+    @Email
+    @NotNull(message = "email is null")
+    private String email;
+
+    @NotNull
+    @Size(min = 8)
+    private String password;
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String getName() {
         return name;
@@ -21,11 +59,4 @@ public class User {
         this.lastname = lastname;
     }
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
