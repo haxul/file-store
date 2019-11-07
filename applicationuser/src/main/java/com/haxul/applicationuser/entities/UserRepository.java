02adc0e45fbada6ac0d2e5fa5aff43cfca14b7c5
haxul <s.s.starodubov@yandex.ru>
2019-11-07 21:24:36 +0400

    init

--- applicationuser/src/main/java/com/haxul/applicationuser/entities/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.haxul.applicationuser.entities;
-
-import org.springframework.data.annotation.Id;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
-}
