584402435bdf056ac8445e40be5ba2e7f334ad90
haxul <s.s.starodubov@yandex.ru>
2019-11-18 23:15:03 +0400

    fix auth.ts

--- client/src/pages/components/auth.ts
+++ client/src/pages/components/auth.ts
@@ -7,6 +7,7 @@ export const Wrapper = styled.div`
 export const Button = styled.button`
   text-align:center;
   width:142px;
+  background: #664956;
 `
 export const Block = styled.div`
   text-align:center;
