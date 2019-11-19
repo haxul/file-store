6b8b0b25b1a776e891d7028badbaa19f06b7b2cd
haxul <s.s.starodubov@yandex.ru>
2019-11-19 20:34:16 +0400

    add icon in project

--- client/src/pages/main/paths/Navbar.tsx
+++ client/src/pages/main/paths/Navbar.tsx
@@ -1,10 +1,10 @@
 import React, { PureComponent } from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
+import icon from "../../../icons/linux.png";
 
 const Span = styled.span`
   cursor: pointer;
-
   :hover {
     color: #664956;
   }
@@ -17,6 +17,7 @@ class Navbar extends PureComponent {
   render() {
     return (
       <Nav className="nav justify-content-end">
+        <img src={icon} alt="icon"/>
         <li className="nav-item">
           <Span className="nav-link ">Log out</Span>
         </li>
