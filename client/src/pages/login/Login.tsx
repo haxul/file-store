286f3c5929e9fd568473054b9646ae1ea6bb4261
haxul <s.s.starodubov@yandex.ru>
2019-11-21 22:59:15 +0400

    fix Login

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,42 +1,75 @@
 import React, { PureComponent } from "react"
 import { connect } from "react-redux"
 import { Wrapper, Button, Block } from "../components/auth"
-import getToken from "../../services/getToken";
+import getToken from "../../services/getToken"
+import { setLoginUsername, setPasswordUsername } from "../../actions/login"
+import styled from "styled-components"
+import { LoginStateProps, LoginDispatchProps } from "./login.interface"
+import { Redirect } from "react-router-dom"
 
-class Login extends PureComponent {
-
+const Error: any = styled.div`
+  display: ${(props: any): any => props.show};
+  color: red;
+  text-align: center;
+  width: 100%;
+`
+const P = styled.div`
+  padding-top: 4px;
+`
+class Login extends PureComponent<any> {
   state = {
-    username: "",
-    password: ""
+    error: false,
+    redirect: false
   }
 
-  getJwtToken = async () => {
-    const { username, password } = this.state
-    const token:string = await getToken(username, password)
-    if (!token) this.setState({error: true })
+  getJwtToken = async (): Promise<void> => {
+    const { username, password } = this.props.login
+    this.setState({ error: false })
+    const token = await getToken(username, password)
+    if (!token) {
+      this.setState({ error: true })
+      return
+    }
     localStorage.setItem("token", token)
+   this.setState({redirect: true})
   }
 
   render() {
+    const { setLoginUsername, setPasswordUsername } = this.props
+    const { error, redirect } = this.state
+    if (redirect) return <Redirect to={"/"}/>
+
     return (
       <Wrapper className="container">
         <div className="row justify-content-center">
           <Block className="col-sm-4">
+            <input type="username" placeholder="enter username" onChange={e => setLoginUsername(e.target.value)} />
             <input
-              type="username"
-              placeholder="enter username"
-              onChange={e => this.setState({ username: e.target.value })}
+              type="password"
+              className="my-2"
+              placeholder="password"
+              onChange={e => setPasswordUsername(e.target.value)}
             />
-            <input type="password" className="my-2" placeholder="password" onChange={e => this.setState({ password: e.target.value })} />
             <Button type="button" className="btn btn-warning" onClick={this.getJwtToken}>
-              log in
+              {" "}
+              log in{" "}
             </Button>
+            <P>
+              {" "}
+              not registered? <a href="/signup">sign up</a>
+            </P>
+            <Error show={error ? "block" : "none"}>Error!!!</Error>
           </Block>
-
         </div>
       </Wrapper>
     )
   }
 }
 
-export default connect()(Login)
+const mapStateToProps = (state:any): LoginStateProps => ({ login: state.login })
+
+const mapDispatchToProps: LoginDispatchProps = {
+  setLoginUsername,
+  setPasswordUsername,
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
