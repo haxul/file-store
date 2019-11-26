6c3830253fdb8664a79e623fa25e4ba43ec27043
haxul <s.s.starodubov@yandex.ru>
2019-11-26 22:37:15 +0400

    fix navbar

--- client/src/pages/main/paths/Navbar.tsx
+++ client/src/pages/main/paths/Navbar.tsx
@@ -1,29 +1,47 @@
-import React, { PureComponent } from "react";
-import { connect } from "react-redux";
-import styled from "styled-components";
-import icon from "../../../icons/linux.png";
+import React, { PureComponent } from "react"
+import { connect } from "react-redux"
+import styled from "styled-components"
+import icon from "../../../icons/linux.png"
+import { withRouter } from "react-router-dom"
 
 const Span = styled.span`
   cursor: pointer;
   :hover {
     color: #664956;
   }
-`;
+`
 const Nav = styled.ul`
   background: #ffc107;
-`;
+`
 
-class Navbar extends PureComponent {
+class Navbar extends PureComponent<any> {
+  
+  async componentDidMount() {
+    const token = localStorage.getItem("token")
+    try {
+      const response = await fetch("http://localhost:8080/token", { method: "GET", headers: { authorization: `Bearer ${token}` } })
+    } catch (error) {
+      console.log(error );
+      
+      // this.props.history.push("/login");
+    }
+  }
+
+  logout = () => {
+    localStorage.removeItem("token")
+    this.forceUpdate()
+  }
+
   render() {
     return (
       <Nav className="nav justify-content-end">
-        <img src={icon} alt="icon"/>
-        <li className="nav-item">
+        <img src={icon} alt="icon" />
+        <li className="nav-item" onClick={this.logout}>
           <Span className="nav-link ">Log out</Span>
         </li>
       </Nav>
-    );
+    )
   }
 }
 
-export default connect()(Navbar);
+export default withRouter(connect()(Navbar))
